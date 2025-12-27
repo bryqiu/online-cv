4bc918ebebedad0fe6dfe101038737ce8e287781
fifteen <qfifteen@163.com>
2025-12-27 18:48:07 +0800

    feat: 添加wx图标组件并更新配置

--- src/constant/icon.ts
+++ src/constant/icon.ts
@@ -5,6 +5,7 @@ import GithubIcon from '@/components/icons/github-icon'
 import InstagramIcon from '@/components/icons/instagram-icon'
 import JueJinIcon from '@/components/icons/jue-jin-icon'
 import LinkedInIcon from '@/components/icons/linkedin-icon'
+import WeChatIcon from '@/components/icons/wechat-icon'
 import XIcon from '@/components/icons/x-icon'
 
 export const SOCIAL_ICON_MAP: Record<SocialPlatform | CommunicationType, React.ComponentType<React.SVGProps<SVGSVGElement>>> = {
@@ -16,5 +17,5 @@ export const SOCIAL_ICON_MAP: Record<SocialPlatform | CommunicationType, React.C
   blog: BlogIcon,
   email: Mail,
   phone: Phone,
-  wx: Phone,
+  wx: WeChatIcon,
 }

d377742d9064e8e99a7524b44eefa0116ed70743
fifteen <qfifteen@163.com>
2026-01-16 15:45:05 +0800

    style(projects): 优化projects布局和样式

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,6 +1,7 @@
 import type { Project } from '@/types'
 import { Star } from 'lucide-react'
 import Image from 'next/image'
+import { cn } from '@/lib/utils'
 import { Badge } from './ui/badge'
 import Anchor from './widgets/anchor'
 import Section from './widgets/section'
@@ -11,8 +12,8 @@ function Projects({ projects}: { projects: Project[] }) {
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         {
           projects.map((project, index) => (
-            <div key={index} className="bg-card rounded-md p-3 border flex flex-col gap-y-1">
-              <div className="flex items-start justify-between gap-2">
+            <div key={index} className="bg-card rounded-md p-3 border flex flex-col gap-y-2">
+              <div className={cn({ 'flex items-start justify-between gap-2': project.stars })}>
                 <div className="flex items-center gap-x-1 flex-1">
                   { project.logo && typeof project.logo === 'string'
                     ? (
@@ -21,17 +22,21 @@ function Projects({ projects}: { projects: Project[] }) {
                     : (
                         project.logo
                       )}
-                  <Anchor href={project.url} text={project.name} className="text-sm font-semibold w-full" textClassName="line-clamp-1" />
+                  <Anchor href={project.url} text={project.name} className="text-sm font-semibold w-full" />
                 </div>
-                <div className="flex items-center gap-x-1">
-                  <Star className="size-3" />
-                  <span className="text-xs">{project.stars}</span>
-                </div>
+                {
+                  project.stars && (
+                    <div className="flex items-center gap-x-1">
+                      <Star className="size-3" />
+                      <span className="text-xs">{project.stars}</span>
+                    </div>
+                  )
+                }
               </div>
-              <span className="text-xs text-card-foreground/70 line-clamp-2">{project.description}</span>
+              <span className="text-xs text-card-foreground/80 line-clamp-2 flex-1">{project.description}</span>
               <div className="flex items-center gap-x-1 flex-wrap">
                 {project.techStack?.map(tech => (
-                  <Badge key={tech} className="py-0" variant="outline">{tech}</Badge>
+                  <Badge key={tech} className="py-0 text-[0.65rem]" variant="secondary">{tech}</Badge>
                 ))}
               </div>
             </div>
